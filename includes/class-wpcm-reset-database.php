c785168cff144ccf2934fb6b9619f091f69b8a8d
Iain <iain@polevaultweb.com>
2024-04-11 08:26:15 +0100

    Fix WordPress database error You have an error in your SQL syntax on uninstall

--- includes/class-wpcm-reset-database.php
+++ includes/class-wpcm-reset-database.php
@@ -35,7 +35,7 @@ class WPCM_Reset_Database {
 		$this->delete_terms( 'wpcm_team' );
 		$this->delete_terms( 'wpcm_venue' );
 
-		$wpdb->query( "DELETE FROM meta {$wpdb->postmeta} meta INNER JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE posts.post_type IN ( 'wpcm_player', 'wpcm_staff', 'wpcm_club', 'wpcm_match', 'wpcm_sponsor', 'wpcm_roster', 'wpcm_table' );" );
+		$wpdb->query( "DELETE meta FROM {$wpdb->postmeta} meta INNER JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE posts.post_type IN ( 'wpcm_player', 'wpcm_staff', 'wpcm_club', 'wpcm_match', 'wpcm_sponsor', 'wpcm_roster', 'wpcm_table' );" );
 		$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'wpcm_player', 'wpcm_staff', 'wpcm_club', 'wpcm_match', 'wpcm_sponsor', 'wpcm_roster', 'wpcm_table' );" );
 
 		delete_option( 'wpclubmanager_installed' );
